5d85198167725d33e0dff52151fe108843398cdb
Szymon Bartnik <szymbar099@student.polsl.pl>
2016-10-15 13:44:16 +0200

    Modified WebApi project and added Mobile App (Cordova project)

--- WebApi/Properties/AssemblyInfo.cs
+++ WebApi/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4bf757d4-c8d4-416f-bdb1-95f308dd1ede")]
+[assembly: Guid("cb2a3f6e-aa55-47e6-b2ed-20c5552cd107")]
 
 // Version information for an assembly consists of the following four values:
 //
