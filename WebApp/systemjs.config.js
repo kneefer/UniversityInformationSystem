05024c579b37bb8a213a961ec1c3b36d428eefda
Szymon Bartnik <szymon.bartnik@krollontrack.com>
2016-11-18 18:25:55 +0100

    Cosmetic changes.

--- WebApp/systemjs.config.js
+++ WebApp/systemjs.config.js
@@ -4,13 +4,14 @@
 (function (global) {
     System.config({
         paths: {
-            // paths serve as alias
             'npm:': 'node_modules/'
         },
+
         // map tells the System loader where to look for things
         map: {
             // our app is within the app folder
-            app: 'app',
+        	app: 'app',
+
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -20,10 +21,12 @@
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
             // other libraries
             'rxjs': 'npm:rxjs',
             'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
         },
+
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
         	app: {
@@ -33,10 +36,6 @@
         	rxjs: {
         		defaultExtension: 'js'
         	},
-        	'angular2-in-memory-web-api': {
-        		main: './index.js',
-        		defaultExtension: 'js'
-        	}
         }
     });
 })(this);
