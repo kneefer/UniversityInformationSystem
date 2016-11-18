1f338d14e0cd823987a11d7cd2b33db5ca34cb36
Szymon Bartnik <szymon.bartnik@krollontrack.com>
2016-11-18 19:32:36 +0100

    Update of TS plugin working.

--- WebApp/app/login/login.service.ts
+++ WebApp/app/login/login.service.ts
@@ -2,9 +2,9 @@
 import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/do.js';
-import 'rxjs/add/operator/catch.js';
-import 'rxjs/add/operator/map.js';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 import { IEntity } from '../shared/models/entity';
 
--- WebApp/app/paneluser/paneluser.service.ts
+++ WebApp/app/paneluser/paneluser.service.ts
@@ -2,7 +2,9 @@
 import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
-
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 import { IEntity } from '../shared/models/entity';
 
