020344eb22673729ba0ec21118f50570eaa448ce
Szymon Bartnik <szymon.bartnik@krollontrack.com>
2016-11-19 16:06:45 +0100

    Added some code to test login screen.

--- WebApp/app/login/login.service.ts
+++ WebApp/app/login/login.service.ts
@@ -1,5 +1,6 @@
 ﻿import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
@@ -8,7 +9,43 @@ import 'rxjs/add/operator/map';
 
 import { IEntity } from '../shared/models/entity';
 
+export class User {
+	constructor(
+		public email: string,
+		public password: string) { }
+}
+
+var users = [
+	new User('admin@admin.com', 'adm9'),
+	new User('user1@gmail.com', 'a23')
+];
+
 @Injectable()
 export class LoginService {
+
+	constructor(
+		private _router: Router) { }
+
+	logout() {
+		localStorage.removeItem("user");
+		this._router.navigate(['Login']);
+	}
+
+	login(user) {
+		//var authenticatedUser = users.find(u => u.email === user.email);
+		//if (authenticatedUser && authenticatedUser.password === user.password) {
+		//	localStorage.setItem("user", authenticatedUser);
+		//	this._router.navigate(['Home']);
+		//	return true;
+		//}
+		//return false;
+
+	}
+
+	checkCredentials() {
+		if (localStorage.getItem("user") === null) {
+			this._router.navigate(['Login']);
+		}
+	} 
 
 }
