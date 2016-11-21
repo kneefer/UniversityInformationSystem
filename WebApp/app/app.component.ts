ac0434738527d5c9eea998fe3d0d971435a0ff74
Szymon <szymbar099@student.polsl.pl>
2016-11-21 23:04:28 +0100

    Cosmetic change with page title service initialization.

--- WebApp/app/app.component.ts
+++ WebApp/app/app.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 
 import { PageTitleService } from './core/pagetitle.service';
 
@@ -7,11 +7,14 @@ import { PageTitleService } from './core/pagetitle.service';
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
     public title : string = "DEFAULT_TITLE";
 
-    constructor(private pageTitleService: PageTitleService) {
+    constructor(
+        private pageTitleService: PageTitleService) { }
+
+    ngOnInit(): void {
         this.pageTitleService.name.subscribe(newTitle => this.title = newTitle);
     }
 }
