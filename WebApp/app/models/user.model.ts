9826503822b33a59f9987e0704b1d7bfdb65a881
Szymon <szymbar099@student.polsl.pl>
2016-11-24 00:36:12 +0100

    Working on show/edit user-tile control.

--- WebApp/app/models/user.model.ts
+++ WebApp/app/models/user.model.ts
@@ -1,6 +1,7 @@
 ﻿export class User {
 
     constructor(
+        public id: string,
         public firstName: string,
         public lastName: string,
         public userName: string,
--- WebApp/app/panel-admin/users/users.component.ts
+++ WebApp/app/panel-admin/users/users.component.ts
@@ -31,20 +31,20 @@ export class PanelAdminUsersComponent implements OnInit {
     public ngOnInit(): void {
         this.pageTitleService.name.next('Panel Admin - Users');
 
-        this.users.push(new User('Adam', 'Kowalski', 'adakow123', 'akow@fdsf.com', 'nothing'));
-        this.users.push(new User('Tomasz', 'Nowak', 'tomnow354', 'sdffd@fdsf.com', ''));
-        this.users.push(new User('Jarosław', 'Paduch', 'jaropad3434', 'df3@fd.com', ''));
-        this.users.push(new User('Agnieszka', 'Debudaj', 'agadebu34', 'df@df.com', 'description'));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
-        this.users.push(new User('Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('1', 'Adam', 'Kowalski', 'adakow123', 'akow@fdsf.com', 'nothing'));
+        this.users.push(new User('2', 'Tomasz', 'Nowak', 'tomnow354', 'sdffd@fdsf.com', ''));
+        this.users.push(new User('3', 'Jarosław', 'Paduch', 'jaropad3434', 'df3@fd.com', ''));
+        this.users.push(new User('4', 'Agnieszka', 'Debudaj', 'agadebu34', 'df@df.com', 'description'));
+        this.users.push(new User('5', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('6', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('7', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('8', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('9', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('10', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('11', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('12', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('13', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('14', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
+        this.users.push(new User('15', 'Wojciech', 'Jeziorski', 'wojcjez3434', 'sdfdf@com.com', ''));
     }
 }
