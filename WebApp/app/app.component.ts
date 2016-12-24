da0608ad33e522d7d76c101c18e66b228ddfd6bb
Szymon Bartnik <szymon.bartnik@krollontrack.com>
2016-12-24 01:58:24 +0100

    Added full screen mode when in preview views.

--- WebApp/app/app.component.ts
+++ WebApp/app/app.component.ts
@@ -1,4 +1,5 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
 import { PageTitleService } from './core/page-title.service';
@@ -15,24 +16,40 @@ declare var module: { id: string; }
     templateUrl: 'app.html',
     styleUrls: ['app.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterContentInit {
 
     public title = '';
-    public currentYear = 0;
+	public currentYear = 0;
+	public isFullScreenMode = false;
 
     constructor(
-        private pageTitleService: PageTitleService,
+		private pageTitleService: PageTitleService,
+		private router: Router,
         private titleService: Title,
         private isAdminGuard: IsAdminGuard,
         private isUserGuard: IsUserGuard,
         private authGuard: IsAuthGuard) { }
 
-    public ngOnInit(): void {
-        this.pageTitleService.name.subscribe(newTitle => {
-            this.title = newTitle;
-            this.titleService.setTitle(`University Information System - ${newTitle}`);
-        });
+	public ngOnInit(): void {
+		this.pageTitleService.name.subscribe(newTitle => {
+			this.title = newTitle;
+			this.titleService.setTitle(`University Information System - ${newTitle}`);
+		});
 
-        this.currentYear = new Date().getFullYear();
-    }
+		this.currentYear = new Date().getFullYear();
+	}
+
+	public ngAfterContentInit(): void {
+		this.router.events.subscribe(s => {
+			if (s instanceof NavigationEnd) {
+				this.navigationChanged();
+			}
+		});
+	}
+
+	private navigationChanged(): void {
+		const currentUrlTree = this.router.parseUrl(this.router.url);
+		const children = currentUrlTree.root.children['primary'];
+		this.isFullScreenMode = children.segments[0].path == "preview";
+	}
 }
