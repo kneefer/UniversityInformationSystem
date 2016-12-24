8c67e2ce2a1d272e2cf33c6e03e1de6868783a95
Szymon Bartnik <szymon.bartnik@krollontrack.com>
2016-12-24 22:47:24 +0100

    Some progress.

---
+++ Utils/restore_remote.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\MongoDB\Server\3.2\bin\mongo" uisdb.westeurope.cloudapp.azure.com UISDB --eval "db.dropDatabase()"
+"C:\Program Files\MongoDB\Server\3.2\bin\mongorestore" %~dp0Dump
