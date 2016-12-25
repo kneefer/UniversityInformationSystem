8454d2ec152b8f706d363d1915bd1e3699799fd7
Szymon Bartnik <szymon.bartnik@krollontrack.com>
2016-12-25 04:12:03 +0100

    Deployed correctly

--- Utils/restore_remote.bat
+++ Utils/restore_remote.bat
@@ -1,2 +1,1 @@
-"C:\Program Files\MongoDB\Server\3.2\bin\mongo" uisdb.westeurope.cloudapp.azure.com UISDB --eval "db.dropDatabase()"
-"C:\Program Files\MongoDB\Server\3.2\bin\mongorestore" %~dp0Dump
+"C:\Program Files\MongoDB\Server\3.2\bin\mongo" uisdb.westeurope.cloudapp.azure.com:27017/UISDB default_data.js
