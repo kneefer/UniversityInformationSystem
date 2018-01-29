c6c3c6f8073bd5413e23e1c2f9faa8dcdee6aa91
Szymon Bartnik <szymbar099@student.polsl.pl>
2018-01-29 23:47:29 +0100

    Added swagger.

--- UIS.Backend/UIS.Backend.Api/Startup.cs
+++ UIS.Backend/UIS.Backend.Api/Startup.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 using UIS.Backend.Api.Extensions;
 
 namespace UIS.Backend.Api
@@ -19,11 +22,26 @@ namespace UIS.Backend.Api
       // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services)
       {
-         services.AddAuthentication(sharedOptions =>
+         services.AddAuthentication(authOptions =>
             {
-               sharedOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+               authOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
             })
             .AddAzureAdB2CBearer(options => Configuration.Bind("AzureAdB2C", options));
+
+         services.AddSwaggerGen(swaggerOptions =>
+         {
+            swaggerOptions.SwaggerDoc("v1", new Info
+            {
+               Version = "v1",
+               Title = "University Information System APIs",
+               Contact = new Contact { Name = "Szymon Bartnik", Email = "szbartnik@gmail.com", Url = "https://github.com/kneefer" }
+            });
+
+            // Set the comments path for the Swagger JSON and UI.
+            var basePath = AppContext.BaseDirectory;
+            var xmlPath = Path.Combine(basePath, "UIS.Backend.Api.xml");
+            swaggerOptions.IncludeXmlComments(xmlPath);
+         });
 
          services.AddMvc();
       }
@@ -33,8 +51,14 @@ namespace UIS.Backend.Api
       {
          if (env.IsDevelopment())
          {
+            app.UseSwagger();
             app.UseDeveloperExceptionPage();
          }
+
+         app.UseSwaggerUI(c =>
+         {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "UIS Global");
+         });
 
          app.UseAuthentication();
          app.UseMvc();
